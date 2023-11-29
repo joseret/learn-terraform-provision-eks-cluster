8e12fb667123ca23e943498f3f289dcd72b56b90
Jose Retelny <joseret@google.com>
2023-11-29 13:38:17 -0800

    pat

--- jr_extra/helm1/addons.tf
+++ jr_extra/helm1/addons.tf
@@ -11,7 +11,7 @@ resource "helm_release" "nginx" {
 # resource "helm_release" "aws-calico" {
 #   name       = "aws-calico"
 #   repository = "https://docs.tigera.io/calico/charts"
-#   repository = "https://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@raw.githubusercontent.com/joseret/jr-cb1-helm-v1/main/charts/"
+#   repository = "https://pat@raw.githubusercontent.com/joseret/jr-cb1-helm-v1/main/charts/"
 # 
 #   chart      = "tigera-operator"
 
