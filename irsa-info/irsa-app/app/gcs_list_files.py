3899b75cd1b1cfd3a73c2231507dc92ef2c63d64
Jose Retelny <joseret@google.com>
2024-05-20 11:17:37 -0700

    fix acm - bin auth

--- irsa-info/irsa-app/app/gcs_list_files.py
+++ irsa-info/irsa-app/app/gcs_list_files.py
@@ -76,6 +76,40 @@ def list_blobs(bucket_name):
 #   .build();
 
 #  eval $(aws sts assume-role-with-web-identity --role-arn $AWS_ROLE_ARN --role-session-name ${session_name} --web-identity-token $(cat $AWS_WEB_IDENTITY_TOKEN_FILE) | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+from google.auth import aws
+from google.auth import exceptions
+
+class CustomAwsSecurityCredentialsSupplier(aws.AwsSecurityCredentialsSupplier):
+
+    def get_aws_security_credentials(self, context, request):
+        audience = context.audience
+        try:
+            # Return valid AWS security credentials. These credentials are not cached by
+            # the google credential, so caching should be implemented in the supplier.
+            r = get_session_token()
+            ACCESS_KEY_ID = r['Credentials']['AccessKeyId']
+            SECRET_ACCESS_KEY = r['Credentials']['SecretAccessKey']
+            SESSION_TOKEN = r['Credentials']['SessionToken']
+            return aws.AwsSecurityCredentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY, SESSION_TOKEN)
+        except Exception as e:
+            # If credentials retrieval fails, raise a refresh error, setting retryable to true if the client should
+            # attempt to retrieve the subject token again.
+            raise exceptions.RefreshError(e, retryable=True)
+
+    def get_aws_region(self, context, request):
+        # Return active AWS region.
+        return os.environ['AWS_DEFAULT_REGION']
+
+
+
+supplier = CustomAwsSecurityCredentialsSupplier()
+AUDIENCE= "//iam.googleapis.com/projects/496684231732/locations/global/workloadIdentityPools/aws-brcm-irsa-pool-v1/providers/aws-brcm-irsa-pool-v1-ap1"
+credentials = aws.Credentials(
+    AUDIENCE, # Set GCP Audience.
+    "urn:ietf:params:aws:token-type:aws4_request", # Set AWS subject token type.
+    aws_security_token_supplier=supplier, # Set supplier.
+    # scopes=SCOPES # Set desired scopes.
+)
 
 def get_session_token():
   try:
